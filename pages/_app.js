6f2214c352ec897df5e56d9c742552a2ddeb671b
Md Abul Faez <floatingfaez@live.com>
2022-07-29 20:52:14 +0600

    loader spin and load first view

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,14 @@ import { useEffect, useState } from "react";
 import Router from 'next/router';
 import NProgress from "nprogress";
 import { ThemeProvider } from "next-themes";
-// import Loader from "@components/ui/loader";
+import { randomBetween, clamp } from "@utils/all";
 import "../css/tailwind.css";
 
-function clamp(n, min, max) {
-  if (n < min) return min;
-  if (n > max) return max;
-  return n;
-}
 
 
 function MyApp({ Component, pageProps }) {
   const [count, setCount] = useState(0)
+  const [seconds, setSeconds] = useState(0)
 
   const startCount = function() {
     if (!NProgress.status) setCount(0);
@@ -28,40 +24,82 @@ function MyApp({ Component, pageProps }) {
         }
         n = clamp(n + amount, 0, 0.994);
         let percentage = n * 1000
-        setCount(percentage)
-        if(!!document){
-          document.getElementById('percentage').textContent = percentage.toFixed(1)
+        if(percentage < 101){
+          setCount(percentage)
+        }
+        if(!!document && percentage < 101){
+          document.getElementById('percentage').textContent = percentage.toFixed(0)
         }
         work();
       }, 200);
     };
-
     if (NProgress.trickle) work();
-
     return this;
   }
   
   NProgress.configure({
     showSpinner: false,
-    template: `<div class="page-loader h-screen w-full theme-bg-black"><div class="bar" role="bar"><div class="peg"></div></div><div class="logo"><img src="/img/footer-logo.svg" width="115" height="115"/><p class="font-secondary mt-8 uppercase text-center fss-2 text-white">Loading <span id='percentage'>${count.toFixed(1)}</span>%</p></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>`
+    template: `<div class="page-loader h-screen w-full theme-bg-black"><div class="bar" role="bar"><div class="peg"></div></div><div class="logo"><img src="/img/footer-logo.svg" width="115" height="115"class="animate-[spin_6s_linear_infinite]"/><p class="font-secondary mt-8 uppercase text-center fss-2 text-white">Loading <span id='percentage'>${count.toFixed(0)}</span>%</p></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>`
   });
+
+  useEffect(()=>{
+    NProgress.start()
+    startCount()
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 800);
+
+    if(seconds === 2){
+      clearInterval(interval)
+      if(!!document){
+        document.getElementById('percentage').textContent = randomBetween(96,100).toFixed(0)
+      }
+      setTimeout(()=>{
+        NProgress.done()
+      },500)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  },[seconds])
 
 
   useEffect(() => {
-    
     Router.events.on("routeChangeStart", () =>{
       startCount()
       NProgress.start()
     });
-    Router.events.on("routeChangeComplete", () => NProgress.done());
-    Router.events.on("routeChangeError", () => NProgress.done());
+    Router.events.on("routeChangeComplete", () => {
+      if(!!document){
+        document.getElementById('percentage').textContent = randomBetween(96,100)
+      }
+      setTimeout(()=>{
+        NProgress.done()
+      },500)
+    });
+    Router.events.on("routeChangeError", () => {
+      if(!!document){
+        document.getElementById('percentage').textContent = randomBetween(96,100)
+      }
+      setTimeout(()=>{
+        NProgress.done()
+      },500)
+    });
 
     return () => {
       Router.events.off("routeChangeStart", () =>{
         NProgress.start()
-        setCount(0)
+        startCount()
       });
-      Router.events.off("routeChangeComplete", () => NProgress.done());
+      Router.events.off("routeChangeComplete", () => {
+        if(!!document){
+          document.getElementById('percentage').textContent = randomBetween(96,100)
+        }
+        setTimeout(()=>{
+          NProgress.done()
+        },500)
+      });
       Router.events.off("routeChangeError", () => NProgress.done());
     }
   }, [])
--- utils/all.js
+++ utils/all.js
@@ -6,3 +6,14 @@ export const cx = (...classNames) =>
 export const myLoader = ({ src }) => {
   return src;
 };
+
+
+export const clamp = (n, min, max) => {
+  if (n < min) return min;
+  if (n > max) return max;
+  return n;
+}
+
+export const randomBetween = (min, max) => { 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
