4f6ee2af9523a8611dd4f1fb08e64bd4387fc576
Md Abul Faez <floatingfaez@live.com>
2022-08-01 09:37:10 +0600

    fix tap height issue

--- components/ui/tape-border.js
+++ components/ui/tape-border.js
@@ -7,13 +7,13 @@ const TapeBorder = ({scrollText,count=5}) => {
             <Marquee count={count} classes="tape-p pos-top"  speed={10} direction="right" keyText="mrq_1tX_1_t">
                 {scrollText}
             </Marquee>
-            <Marquee count={count} classes="tape-p pos-right z-10" speed={.5} direction="right" keyText="mrq_1tX_2_r">
+            <Marquee count={count} classes="tape-p pos-right z-10" speed={.3} direction="right" keyText="mrq_1tX_2_r">
                 {scrollText}
             </Marquee>
             <Marquee count={count} classes="tape-p pos-bottom z-30" speed={10}  keyText="mrq_1tX_3_b">
                 {scrollText}
             </Marquee>
-            <Marquee count={count} classes="tape-p pos-left z-20" speed={.5} keyText="mrq_1tX_4_l">
+            <Marquee count={count} classes="tape-p pos-left z-20" speed={.3} keyText="mrq_1tX_4_l">
                 {scrollText}
             </Marquee>
         </Fragment>
