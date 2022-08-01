ad70c50475e10f0f88b008eda155b65b257d9731
Md Abul Faez <floatingfaez@live.com>
2022-08-01 09:16:12 +0600

    subscription modal issue

--- components/modal/subscriptionModal.js
+++ components/modal/subscriptionModal.js
@@ -8,8 +8,8 @@ const SubscriptionModal = ({setModalShow,scrollText,classes=''}) => {
                 <button type="button" className="btn-close" onClick={() => setModalShow(false)}>
                     <img src="/img/cross.svg" alt="cross"/>
                 </button>
-                <div className="modal-body">
-                    <HeaderSection title="USA / Mexico" subtitle='Register Your Interest In' classes="border-0 mb-16 text-white !pb-12 !pt-11"/>
+                <div className="modal-body flex flex-col justify-between h-full">
+                    <HeaderSection title="USA / Mexico" subtitle='Register Your Interest In' classes="border-0 mb-16 text-white !pb-12 !pt-8"/>
                     <NewsletterSubscribe className='max-w-xs mx-auto  px-5 pb-8' type='vertical' />
                 </div>
                 <div className="tape-wrapper">
